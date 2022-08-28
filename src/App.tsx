353229c08a4ad8f2416b4443892a7272c6443468
Kotkoa <kotkoa@gmail.com>
2022-08-28 16:59:51 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Todos from './components/Todos'
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Todos items={['Learn React', 'Learn Typescript']} />
+    </div>
+  )
 }
 
 export default App
